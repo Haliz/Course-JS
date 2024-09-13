c71687b72d64ff286d94d86331f40fbdf565fa35
Haliz <foril2n1@rambler.ru>
2024-09-13 23:49:07 +0300

    Объекты, деструктуризация объектов

--- js/script.js
+++ js/script.js
@@ -1,79 +1,39 @@
 'use strict';
-/* Задание на урок:
 
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
-// Код возьмите из предыдущего домашнего задания
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let lang = '';
+        languages.forEach(value => lang += " " + value.toUpperCase());
+        return `Мне ${age} и я владею языками:${lang}`
     }
-}
-
-start();
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
 };
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+ console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
 }
 
-rememberMyFilms();
+console.log(showExperience(personalPlanPeter));
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
+function showProgrammingLangs(plan) {
+    let rsl = '';
+    const {programmingLangs} = plan.skills
+    for (let lang in programmingLangs) {
+        rsl += `Язык ${lang} изучен на ${programmingLangs[lang]}\n`;
     }
+    return rsl;
 }
 
-detectPersonalLevel();
-
-
-
-function showMyDB() {
-    if (!personalMovieDB['privat']) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
-function writeYourGenres() {
-    for (let i=1; i<=3; i++) {
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
+console.log(showProgrammingLangs(personalPlanPeter));
